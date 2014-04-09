ec774381776c70a6bd54e1ecdab31adea1ac2db9
Ubuntu <ubuntu@biotools-ubuntu12.(none)>
2014-04-09 16:05:39 +0000

    +Add envars.sh: Set ENVIRONMENT variables to allow scripts to find bioapps executables & libraries

---
+++ envars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#### ENVIRONMENT VARIABLES
+export PATH=/agua/apps/python/2.7.3:$PATH
+export PATH=/agua/apps/samtools/0.1.19:$PATH
+export PYTHONPATH=/usr/local/lib/python2.7/:/usr/local/lib/python2.7/lib-dynload:$PYTHONPATH
+export LD_LIBRARY_PATH=/agua/apps/biobambam/libmaus-0.0.108-release-20140319092837/src/.libs
+
