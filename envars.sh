1b36440c7d984d1f7684538359000629fad54e6e
Ubuntu <ubuntu@biotools-ubuntu12.(none)>
2014-04-09 18:22:59 +0000

    -Inc envars.sh: Added PERL5LIB for PCAP-core and more apps to PATH

--- envars.sh
+++ envars.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 #### ENVIRONMENT VARIABLES
+export PATH=/agua/apps/pcap/0.3.0:$PATH
 export PATH=/agua/apps/python/2.7.3:$PATH
 export PATH=/agua/apps/samtools/0.1.19:$PATH
-export PYTHONPATH=/usr/local/lib/python2.7/:/usr/local/lib/python2.7/lib-dynload:$PYTHONPATH
+export PATH=/agua/apps/pcap/0.2/bin:$PATH
+export PATH=/agua/apps/bwa/0.1.19:$PATH
+export PATH=/agua/apps/biobambam/0.0.129/src:$PATH
+export PYTHONPATH=/usr/local/lib/python2.7/:$PYTHONPATH
+export PYTHONPATH=/usr/local/lib/python2.7/lib-dynload:$PYTHONPATH
 export LD_LIBRARY_PATH=/agua/apps/biobambam/libmaus-0.0.108-release-20140319092837/src/.libs
+export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5:$PERL5LIB
+export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
 
