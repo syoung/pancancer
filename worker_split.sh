a12e42e9ec860520c59b99d2c4925a337c92d5e1
Ubuntu <ubuntu@alignmentworker.(none)>
2014-04-10 00:00:58 +0000

    Moving split_bams.sh code to worker_split.sh

--- worker_split.sh
+++ worker_split.sh
@@ -15,10 +15,14 @@ do
 		echo Splitting $UUID
     	if [ -e /pancanfs/input/$UUID ]; then
     		BAM_FILE=`ls /pancanfs/input/$UUID/*.bam`
-#    		sudo docker run -v /pancanfs:/pancanfs -v /mnt:/mnt icgc-aligner $SPLIT_CODE $BAM_FILE /pancanfs/splits/$UUID --workdir /mnt/$UUID
-	        echo "$SPLIT_CODE $BAM_FILE /pancanfs/splits/$UUID --workdir /mnt/$UUID"
-    		$SPLIT_CODE $BAM_FILE /pancanfs/splits/$UUID --workdir /mnt/$UUID
+    		if [ -z $USE_DOCKER ]; then 
+				CMD_PREFIX="" 
+			else
+				CMD_PREFIX="sudo docker run -v /pancanfs:/pancanfs icgc-aligner"
+			fi
 
+	        echo "$CMD_PREFIX $SPLIT_CODE $BAM_FILE /pancanfs/splits/$UUID --workdir /mnt/$UUID"
+    		$CMD_PREFIX $SPLIT_CODE $BAM_FILE /pancanfs/splits/$UUID --workdir /mnt/$UUID
 
 			if [ $? != 0 ]; then 
 				$SYN_MONITOR errorAssignment $UUID "splitting error"
