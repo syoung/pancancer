f46c85cd22e0ffc87b5166063df2017a440f6ad8
Stuart Young <stuartpyoung@gmail.com>
2014-04-11 03:59:46 -0700

    +Add worker_download.sh: Moved from gt_download.sh, includes call to addSamGroups

---
+++ worker_download.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SYN_MONITOR="/pancanfs/software/synapseICGCMonitor"
+DOWNLOAD_SCRIPT="/agua/apps/bioapps/bin/gt/download.pl"
+OUTPUTDIR="/pancanfs/input"
+KEYFILE="/home/ubuntu/annai-cghub.key"
+
+while :
+do
+    echo "Scanning"
+    UUID=`$SYN_MONITOR getAssignmentForWork ucsc_biofarm todownload`
+    if [ $? != 0 ]; then 
+        echo "Done, sleeping"
+        #exit 0
+        sleep 60
+    else
+        echo Downloading $UUID
+        if [ ! -e /pancanfs/input/$UUID ]; then
+            echo "$DOWNLOAD_SCRIPT --uuid $UUID --outputdir $OUTPUTDIR --keyfile $KEYFILE"
+            $DOWNLOAD_SCRIPT --uuid $UUID --outputdir $OUTPUTDIR --keyfile $KEYFILE
+            if [ $? != 0 ]; then 
+                $SYN_MONITOR errorAssignment $UUID "gtdownload error"
+            else
+                if [ $(ls $OUTPUTDIR/$UUID/*.bam | wc -l) = 1 ]; then
+                BAM_FILE=`ls $OUTPUTDIR/$UUID/*.bam`
+                $SYN_MONITOR addBamGroups $UUID $BAM_FILE
+                    $SYN_MONITOR returnAssignment $UUID
+                else
+                    $SYN_MONITOR errorAssignment $UUID "File not found"
+                fi        
+            fi
+        fi
+    fi
+done
