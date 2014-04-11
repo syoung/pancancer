5576cd38f47dcbdee5c49b889404a419354e3bfa
Stuart Young <stuartpyoung@gmail.com>
2014-04-11 03:12:01 -0700

    -Inc envars.sh: Identical to master branch

--- envars.sh
+++ envars.sh
@@ -12,4 +12,4 @@ export PYTHONPATH=/usr/local/lib/python2.7/lib-dynload:$PYTHONPATH
 export LD_LIBRARY_PATH=/agua/apps/biobambam/libmaus-0.0.108-release-20140319092837/src/.libs
 export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5:$PERL5LIB
 export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
-
+export PERL5LIB=/agua/apps/pcap/PCAP-core/lib:$PERL5LIB
