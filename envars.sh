166ca01c9d3ba033f613429f704011588e7404a6
Stuart Young <stuartpyoung@gmail.com>
2014-04-19 23:46:23 -0700

    -Inc envars.sh: Updated standalone perl settings

--- envars.sh
+++ envars.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#### PERL
+#export PATH=/agua/apps/perl/5.18.2/bin:$PATH
+
+#### GLIBC
+export PATH=/mnt/data/apps/libs/boost/1.39.0/libs:$PATH
+
 #### ENVIRONMENT VARIABLES
 export PATH=/agua/apps/pcap/0.3.0:$PATH
 export PATH=/agua/apps/python/2.7.3:$PATH
@@ -7,9 +13,33 @@ export PATH=/agua/apps/samtools/0.1.19:$PATH
 export PATH=/agua/apps/pcap/0.2/bin:$PATH
 export PATH=/agua/apps/bwa/0.1.19:$PATH
 export PATH=/agua/apps/biobambam/0.0.129/src:$PATH
+
+#### PYTHON PATH
 export PYTHONPATH=/usr/local/lib/python2.7/:$PYTHONPATH
 export PYTHONPATH=/usr/local/lib/python2.7/lib-dynload:$PYTHONPATH
-export LD_LIBRARY_PATH=/agua/apps/biobambam/libmaus-0.0.108-release-20140319092837/src/.libs
+
+#### PERL5LIB
+export PERL5LIB=
+export PERL5LIB=/agua/apps/perl/5.18.2/lib/5.18.2/lib/perl5:$PERL5LIB
+export PERL5LIB=/agua/apps/perl/5.18.2/lib/5.18.2/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
 export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5:$PERL5LIB
 export PERL5LIB=/agua/apps/pcap/0.3.0/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
 export PERL5LIB=/agua/apps/pcap/PCAP-core/lib:$PERL5LIB
+## aliased.pm
+export PERL5LIB=/agua/apps/perl/5.18.2/lib/site_perl/5.18.2:$PERL5LIB
+
+#### LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=
+export LD_LIBRARY_PATH=/mnt/data/apps/libs/boost/1.39.0/libs:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/agua/apps/biobambam/libmaus-0.0.108-release-20140319092837/src/.libs:$LD_LIBRARY_PATH
+
+#### LD_PRELOAD
+export LD_PRELOAD=libc.so.6
+
+
+#/agua/apps/pcap/PCAP-core/lib
+#/agua/apps/pcap/0.3.0/lib/perl5/x86_64-linux-gnu-thread-multi
+#/agua/apps/pcap/0.3.0/lib/perl5
+#/agua/apps/perl/5.18.2/lib/5.18.2/lib/perl5/x86_64-linux-gnu-thread-multi
+#/agua/apps/perl/5.18.2/lib/5.18.2/lib/perl5
+
